ccb9724ff6329b82e18f839bb76246cae899fe99
shuxue051 <shuxue051@gmail.com>
2014-09-24 10:50:25 +0800

    chartkick test: success login test:  failure

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,31 @@
-from flask import Flask
+from flask import Flask,url_for,Blueprint
+from flask import Flask, render_template, request, redirect
+from flask.ext.mongoengine import MongoEngine, MongoEngineSessionInterface
+from flask.ext.login import LoginManager
+from flask.ext.bcrypt import Bcrypt
+import os
+import chartkick
+
 
 app = Flask(__name__)
 from app import views
+
+#chartkick
+ck = Blueprint('ck_page', __name__, static_folder=chartkick.js(), static_url_path='/static')
+app.register_blueprint(ck, url_prefix='/ck')
+app.jinja_env.add_extension("chartkick.ext.charts")
+
+# database connection
+app.config['MONGODB_SETTINGS'] = {'HOST':os.environ.get('MONGOLAB_URI'),'DB': 'FlaskLogin'}
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.debug = os.environ.get('DEBUG',False)
+
+db = MongoEngine(app) # connect MongoEngine with Flask App
+app.session_interface = MongoEngineSessionInterface(db) # sessions w/ mongoengine
+
+# Flask BCrypt will be used to salt the user password
+flask_bcrypt = Bcrypt(app)
+
+# Associate Flask-Login manager with current app
+login_manager = LoginManager()
+login_manager.init_app(app)
--- docs/env.md
+++ docs/env.md
@@ -6,3 +6,9 @@ pip install flask-mail
 pip install flask-sqlalchemy
 pip install flask-wtf
 pip install flup
+
+pip install chartkick
+pip install flask-bcrypt
+pip install flask-mongoengine
+pip install flask-script
+pip install flask-uploads
